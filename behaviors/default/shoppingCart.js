51d57cd506c1f2052da74fe216633ddad7af45af
tan <tan@causeverse.io>
2023-06-13 08:37:44 -0700

    integrated postMessage

--- behaviors/default/shoppingCart.js
+++ behaviors/default/shoppingCart.js
@@ -1,39 +1,50 @@
 class ShoppingCartPawn {
   setup() {
     this.addEventListener("pointerTap", "popUp");
+
+    this.subscribe("popupWindow", "addImage", "addImage");
   }
 
   popUp() {
     // debugger;
-    if (document.getElementById("shopping_cart")) {
-      document.getElementById("shopping_cart").remove();
-      delete this.popupWindow;
+    
+    
+
+    if (!document.getElementById("shopping_cart")) {
+      if (this.popupWindow) {
+        delete this.popupWindow;
+      }
+
+      this.popupWindow = document.createElement("iframe");
+      this.popupWindow.src = "/assets/html/shopping_cart.html";
+
+      this.popupWindow.id = "shopping_cart";
+      this.popupWindow.style.position = "absolute";
+      this.popupWindow.style.top = "20px";
+      this.popupWindow.style.left = "20px";
+      this.popupWindow.style.width = "400px";
+      this.popupWindow.style.height = "400px";
+      this.popupWindow.style.margin = "0px";
+      this.popupWindow.style.zIndex = 1000;
+
+      
+
+      document.body.appendChild(this.popupWindow);
+      console.log(this.popupWindow.contentWindow);
+      this.popupWindow.contentWindow.onload = () => {
+        this.popupWindow.contentWindow.postMessage({ message: "select-card", card:this.actor });
+        this.popupWindow.contentWindow.postMessage({ message: "moveWindow" });
+      }
+    } else {
+      this.popupWindow = document.getElementById("shopping_cart");
+      this.popupWindow.contentWindow.postMessage({ message: "select-card", card:this.actor });
+      this.popupWindow.contentWindow.postMessage({ message: "moveWindow" });
     }
 
-    let cardCount = 0;
-    let mouseX = 20;
-    if (window.top.cardsSelected) cardCount = Object.keys(window.top.cardsSelected).length;
-    if (window.top.mousePosition) mouseX = window.top.mousePosition[0];
-
-    this.popupWindow = document.createElement("iframe");
-    this.popupWindow.src = "/assets/html/shopping_cart.html";
-
-    this.popupWindow.id = "shopping_cart";
-    // this.popupWindow.width = 400;
-    // this.popupWindow.height = 400 + cardCount * 150;
-    this.popupWindow.style.position = "absolute";
-    this.popupWindow.style.top = "20px";
-    this.popupWindow.style.left = (Math.min(window.innerWidth - 400,Math.max(20, mouseX - 200))) + "px";
-    this.popupWindow.style.width = "400px";
-    this.popupWindow.style.height = 235 + 66 * cardCount + "px";
-    this.popupWindow.style.margin = "0px";
-    this.popupWindow.style.zIndex = 1000;
-
-    console.log("left = ", this.popupWindow.style.left);
-
-    document.body.appendChild(this.popupWindow);
-
+    
   }
+
+  
 }
 
 export default {
